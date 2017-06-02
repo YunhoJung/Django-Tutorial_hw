c2806cf6e3193cd22b32b2ead91285c2a8f87932
Jung YunHo <saijdu2198@gmail.com>
2017-06-01 18:18:38 -0700

    add Question Model methods

--- django_app/polls/models.py
+++ django_app/polls/models.py
@@ -4,6 +4,12 @@ class Question(models.Model):
     question_text = models.CharField('질문내용', max_length=200)
     pub_date = models.DateTimeField('발행일자')
 
+    def __str__(self):
+        return self.question_text
+
+    def was_published_recently(self):
+        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
+
 class Choice(models.Model):
     question = models.ForeignKey(
         Question,
